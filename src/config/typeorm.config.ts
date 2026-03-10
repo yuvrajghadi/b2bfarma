8925968220d9284bd6d23105e99de05fd3b93317
yuvrajghadi <ghadiyuvraj08@gmail.com>
2026-03-10 18:11:35 +0530

    refactor: use DATABASE_URL for Render PostgreSQL connection

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,7 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  url: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
   entities: ['src/modules/**/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
